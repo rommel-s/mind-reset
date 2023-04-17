bda2dbd1421bbf47d05144df40dca4bd139269da
Rommel Santos <53845558+rommel-s@users.noreply.github.com>
2023-04-17 19:50:37 -0300

    App main page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,111 @@
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import { useState } from "react";
+
+import TimerCoundtown from "../components/Timer/TimerCountdown";
+import {
+  App,
+  TimerWrapper,
+  ButtonContainer,
+  StartButtonContainer
+} from "../components/Containers/Containers.style";
+import { TimerButton, Start } from "../components/Buttons/Button.style";
+import Modal from "../components/Modal/Modal";
+
+const Home = () => {
+  const [timer, setTimer] = useState(false);
+  const [key, setKey] = useState(0);
+  const [duration, setDuration] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleTimer = () => {
+    setTimer(true);
+    setKey((prevKey) => prevKey + 1);
+    if (duration === null) {
+      setDuration(0);
+    } else {
+      setTimeout(() => {
+        toggleModal(duration);
+      }, convertTimers(duration));
+    }
+  };
+
+  const convertTimers = (timeSet) => {
+    return timeSet * 1000;
+  };
+
+  const setTimeDuration = (timeDuration) => {
+    if (timeDuration === 2) {
+      setDuration(120);
+    } else if (timeDuration === 3) {
+      setDuration(180);
+    } else {
+      setDuration(300);
+    }
+  };
+
+  const toggleModal = (time) => {
+    setShowModal((prev) => !prev);
+    console.log("apareceu o tempo de " + time + " segundos");
+  };
+
+  const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <TimerCoundtown minutes={"00"} seconds={"00"} />;
+    }
+
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+
+    if (seconds <= 9) {
+      return (
+        <TimerCoundtown
+          minutes={minutes}
+          seconds={"0" + (remainingTime % 60)}
+        />
+      );
+    }
+
+    return <TimerCoundtown minutes={minutes} seconds={seconds} />;
+  };
+
+  return (
+    <App>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
+      <TimerWrapper>
+        <CountdownCircleTimer
+          key={key}
+          isPlaying={timer}
+          duration={duration}
+          colors={["#ffffff"]}
+          trailColor={"#86C1F0"}
+          size={220}
+          strokeWidth={12}
+          onComplete={() => ({ shouldRepeat: false, delay: 1 })}
+        >
+          {renderTime}
+        </CountdownCircleTimer>
+      </TimerWrapper>
+
+      <ButtonContainer>
+        <TimerButton onClick={() => setTimeDuration(2)}>
+          2<br />
+          min
+        </TimerButton>
+        <TimerButton onClick={() => setTimeDuration(3)}>
+          3<br />
+          min
+        </TimerButton>
+        <TimerButton onClick={() => setTimeDuration(5)}>
+          5<br />
+          min
+        </TimerButton>
+      </ButtonContainer>
+
+      <StartButtonContainer>
+        <Start onClick={handleTimer}>Iniciar</Start>
+      </StartButtonContainer>
+    </App>
+  );
+};
+
+export default Home;
