ade2ecae852d14bf42af3b16a07d11c1eeeef832
Rommel Santos <53845558+rommel-s@users.noreply.github.com>
2023-04-17 19:49:37 -0300

    Set styles folder

---
+++ src/styles/Theme.jsx
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    primary: "#2F80ED",
+    secondary: "#56CCF2",
+    accent: "#009FE3",
+    white: "#ffffff",
+    white01: "#f3f3f3"
+  },
+  fontSize: {
+    regular: 400,
+    medium: 500,
+    bold: 700
+  }
+};
